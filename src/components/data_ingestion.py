f949eb87566094655204cea8d33f20c71547ab4d
RohitNegi12 <110194754+RohitNegi12@users.noreply.github.com>
2024-02-12 20:52:48 +0530

    data transformation and model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os
 import sys
-from  src.exception import CustomException
+from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransfromation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -39,3 +41,12 @@ class DataIngestion:
             
         except Exception as e:
             raise CustomException(e,sys)
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransfromation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
